76a4154a00db019566a802b0391d527052796779
Maarten <maarten@sumocoders.be>
2020-10-08 22:03:11 +0200

    Project start + Added description

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  data() {
+    return {
+      product: 'Socks',
+      description: 'Dictumst donec quam pharetra elit nullam conubia tempor nascetur luctus, dictum mattis at inceptos turpis fringilla ut primis, in taciti torquent rhoncus tincidunt netus erat mauris. Suspendisse lacinia sociosqu luctus ultricies et id dictumst rhoncus consectetur consequat, class massa pretium commodo habitasse ad varius magna pellentesque, blandit mus urna sem mauris facilisi tincidunt vitae dui. Per neque magna mi duis erat mattis netus sit vestibulum faucibus amet, morbi posuere libero sagittis ad aenean quam viverra varius facilisi sapien dignissim, litora montes inceptos rhoncus nam conubia et scelerisque semper accumsan. Dictum dictumst suspendisse curabitur fringilla taciti mi parturient efficitur, elit nulla dui donec magnis cursus nunc potenti, leo facilisi neque dolor aenean platea netus. Fusce neque sed hendrerit curabitur a vitae vehicula ridiculus molestie libero, finibus metus et massa bibendum vestibulum conubia pellentesque sit fermentum quis, quam integer dis rutrum laoreet diam ante amet habitant. Tellus cursus neque justo feugiat sodales tortor, leo laoreet est convallis ad aliquam nec, phasellus bibendum efficitur viverra egestas.'
+    }
+  }
+})
